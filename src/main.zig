3390e8223a7a09881745eedcd56219e1424cd469
Devin J. Pohly <djpohly@gmail.com>
2025-09-11 19:50:15 -0500

    Initial commit.
    This utility was able to flash archlinux-2025.09.01-x86_64.iso (1.5G) to
    a cheap 8G USB2.0 flash drive in 2m15s (including the sync command
    afterward) when cp and cat and pv were all taking over five minutes!

---
+++ src/main.zig
@@ -0,0 +1,40 @@
+const std = @import("std");
+
+const UsageError = error{Usage};
+
+fn usage(argv0: []const u8, msg: []const u8) anyerror {
+    std.debug.print(
+        \\{s}
+        \\Usage: {s} SRC DEST
+        \\
+    , .{ msg, argv0 });
+    return error.Usage;
+}
+
+pub fn main() !void {
+    var args = std.process.args();
+    const argv0 = args.next() orelse unreachable;
+    const srcpath = args.next() orelse return usage(argv0, "Source path is required");
+    const dstpath = args.next() orelse return usage(argv0, "Destination path is required");
+    const num_bytes = if (args.next()) |arg| try std.fmt.parseUnsigned(usize, arg, 0) else null;
+
+    const dir = std.fs.cwd();
+    const srcfile = try dir.openFileZ(srcpath, .{});
+    defer srcfile.close();
+    const srcstat = try srcfile.stat();
+    const dstfile = try dir.createFileZ(dstpath, .{ .mode = srcstat.mode });
+    defer dstfile.close();
+
+    // No userspace buffer should be needed for sendfile()
+    const no_buffer = [0]u8{};
+    var source = srcfile.reader(&no_buffer);
+    // Using dstfile.writer() results in error.Unimplemented (in 0.15.1)
+    var sink = dstfile.writerStreaming(&no_buffer);
+    const ret = try std.fs.File.Writer.sendFile(
+        &sink.interface,
+        &source,
+        if (num_bytes) |b| .limited(b) else .unlimited,
+    );
+
+    std.debug.print("returned {d}\n", .{ret});
+}
